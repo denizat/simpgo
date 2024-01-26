23a41dcf57aaa661957c338434e3d774a9c60a9d
denizat <deniza.telci@gmail.com>
2024-01-26 16:45:38 -0600

    go fmt

--- testing/tt.go
+++ testing/tt.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"bytes"
+	"fmt"
 )
 
 func main() {
 	var buf bytes.Buffer
 	index(&buf, "richard")
-	fmt.Printf("%s",buf.String())
+	fmt.Printf("%s", buf.String())
 }
